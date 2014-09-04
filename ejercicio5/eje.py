e0e99976d35f8e1cb18e9ae81e810aa5baf7eb15
Hilda <hildapinto@gmail.com>
2014-09-03 17:12:35 -0700

    quinto

---
+++ ejercicio5/eje.py
@@ -0,0 +1,12 @@
+Python 2.7.8 (default, Jun 30 2014, 16:08:48) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> numero = int(input("Ingrese el numero de cual desea ver la tabla de multi\
+plicar: "))
+rango = range (1, 11)
+for elemento in rango :
+producto = numero * elemento
+print (numero , 'x' , elemento , ' = ' , producto)
+print ("Esta fue la tabla de multiplicar del numero elegido")
+Ingrese el numero de cual desea ver la tabla de multiplicar: 7
+>>> 
+>>> 
