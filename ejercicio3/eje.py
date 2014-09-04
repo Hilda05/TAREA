db10284b63e40ab4f5c6122f843c2b5197eeab85
Hilda <hildapinto@gmail.com>
2014-09-03 17:10:50 -0700

    tercero

---
+++ ejercicio3/eje.py
@@ -0,0 +1,11 @@
+Python 2.7.8 (default, Jun 30 2014, 16:08:48) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 'z' in 'radar'
+False
+>>> 'i' in 'casa'
+False
+>>> 'nz' in 'radar'
+False
+>>> 'az' in 'radar'
+False
+>>> 
